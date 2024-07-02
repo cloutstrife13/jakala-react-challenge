d2491d1ff1ab6393ff7d3b4359d23c5ef733cb02
Andy Le <andyle13@outlook.com>
2024-07-02 21:13:56 +0200

    feat: add enum for endpoints

---
+++ src/utils/enum.ts
@@ -0,0 +1,8 @@
+export enum SwapiEndpoint {
+  FILMS = "https://swapi.dev/api/films",
+  PEOPLE = "https://swapi.dev/api/people",
+  PLANETS = "https://swapi.dev/api/planets",
+  SPECIES = "https://swapi.dev/api/species",
+  STARSHIPS = "https://swapi.dev/api/starships",
+  VEHICLES = "https://swapi.dev/api/vehicles",
+}
