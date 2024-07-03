0ce6ec0e424c3aae06de4750db14dd9e3a78838f
Andy Le <andyle13@outlook.com>
2024-07-03 17:44:23 +0200

    feat: set up react router

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   test: {
     environment: "jsdom",
     globals: true,
